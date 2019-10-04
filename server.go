2c0a432fb5b56a03e16da7debedd63ea279e4f1d
d-shimizu <shimizu.daisuke.sd@gmail.com>
2019-10-04 14:31:23 +0000

    Add POST Method

--- server.go
+++ server.go
@@ -9,6 +9,8 @@ func main()	{
 	e := echo.New()
 
     e.GET("/users/:id", getUser)
+	e.GET("/show", show)
+	e.POST("/save", save)
 
 	e.GET("/", func(c echo.Context) error {
 		return c.String(http.StatusOK, "Hello, World!!")
@@ -21,3 +23,14 @@ func getUser(c echo.Context) error {
 	return c.String(http.StatusOK, id)
 }
 
+func show(c echo.Context) error {
+	team := c.QueryParam("team")
+	member := c.QueryParam("member")
+	return c.String(http.StatusOK, "team:" + team + ", member" + member)
+}
+
+func save(c echo.Context) error {
+	name := c.FormValue("name")
+	email := c.FormValue("email")
+	return c.String(http.StatusOK, "name:" + name + ", email:" + email)
+}
