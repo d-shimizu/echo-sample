c081d2a0b1327a43e6100635b40391591bb3dfc9
d-shimizu <shimizu.daisuke.sd@gmail.com>
2019-10-04 14:15:39 +0000

    Add Routing and Path Parameters

--- server.go
+++ server.go
@@ -7,8 +7,17 @@ import (
 
 func main()	{
 	e := echo.New()
+
+    e.GET("/users/:id", getUser)
+
 	e.GET("/", func(c echo.Context) error {
 		return c.String(http.StatusOK, "Hello, World!!")
 	})
 	e.Logger.Fatal(e.Start(":1323"))
 }
+
+func getUser(c echo.Context) error {
+	id := c.Param("id")
+	return c.String(http.StatusOK, id)
+}
+
